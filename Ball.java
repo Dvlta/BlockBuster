2cfe07ea100706230148ff9f92ec4cb9cac2b569
Dvlta <leobluehills2015@gmail.com>
2022-05-02 12:05:00 -0700

    collide methods

--- Ball.java
+++ Ball.java
@@ -1,8 +1,18 @@
 public class Ball{
 
     private int speed;
+    private double angle;
     
     public Ball(){
         speed = 1;
+        angle = 0.0;
+    }
+
+    public void collide(){
+
+    }
+
+    public void collideBottom(){
+        
     }
 }
