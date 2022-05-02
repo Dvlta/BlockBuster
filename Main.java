d82b92c2c83b8aa3a1fa61edac205bfe533bfdb6
Dvlta <leobluehills2015@gmail.com>
2022-05-02 13:05:36 -0700

    new stuff

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
-    
+    public static void main(String[] args){
+        ArrayList<Ball> balls = new ArrayList<>();
+        ArrayList<Block[]> blocks = new ArrayList<>();
+        ArrayList<WhiteCircle> whiteCircles = new ArrayList<>();
+    }
+
 }
---
+++ WhiteCircle.java
@@ -0,0 +1,5 @@
+public class WhiteCircle {
+    public WhiteCircle(){
+        
+    }
+}
