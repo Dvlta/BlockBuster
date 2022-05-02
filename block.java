9223c5cfcee0f098f03cc33b3806f6c388b1ee34
Tristan <triskieranli@gmail.com>
2022-05-02 12:36:50 -0700

    get methods

--- block.java
+++ block.java
@@ -1,5 +1,15 @@
 public class Block {
 private int length = 5;
 private int width = 5;
-    
+    public Block() {
+    }
+
+    public int getBlockLength() {
+        return length;
+    }
+
+    public int getBlockWidth() {
+        return width;
+    }
+
 }
