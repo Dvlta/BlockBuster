40456ec046b1f8732674df0f4d55d06de28c406a
Dvlta <leobluehills2015@gmail.com>
2022-05-09 12:21:19 -0700

    jpanel

--- Main.java
+++ Main.java
@@ -1,11 +1,28 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+public class Main extends JPanel implements Runnable{
+
+    public void run(){
+        
+    }
     public static void main(String[] args){
         ArrayList<Ball> balls = new ArrayList<>();
         ArrayList<Block[]> blocks = new ArrayList<>();
         ArrayList<WhiteCircle> whiteCircles = new ArrayList<>();
+        JFrame myFrame = new JFrame("Space Race");
+        Main myPanel = new Main();
+        myPanel.setPreferredSize(new Dimension(500,800));
+        myPanel.setBackground(Color.BLACK);
+        myFrame.add(myPanel);
+        myFrame.pack();
+        myFrame.setLocationRelativeTo(null);
+        myFrame.setVisible(true);
+        new Thread(myPanel).start();
+
     }
 
 }
