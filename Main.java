be455a98bcd5114fdca59d1845cd8088a5794f83
nkong0 <nkong122@student.fuhsd.org>
2022-05-16 12:25:02 -0700

    new

--- Main.java
+++ Main.java
@@ -24,7 +24,8 @@ public class Main extends JPanel implements Runnable
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        if(!gameOver){
+        if(!gameOver)
+        {
             g.setColor(Color.blue);
             for(int i=0;i<blocks.size();i++){
                 for(int j = 0;j<blocks.get(i).length;j++){
@@ -42,8 +43,10 @@ public class Main extends JPanel implements Runnable
             for(int i=0;i<balls.size();i++){
                 Ball b = balls.get(i);
                 g.drawOval(b.getX()-ballRadius, b.getY()+ballRadius, ballRadius*2, ballRadius*2);
+
+            g.setColor(Color.white);
+            g.drawRect(740, 980, 10, 10);
             }
-            
         }
     }
 
@@ -70,7 +73,7 @@ public class Main extends JPanel implements Runnable
                 int start = (int)(Math.random() * 751) + 5;
                 if (round == 1)
                 {
-                    balls.add
+                    balls.add(new Ball(0, start));
                 }
 
             }
